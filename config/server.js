5100313a03385411869a6001bdf05f5a6dd04732
MSI\Luis Javier <vato_182@hotmail.com>
2021-03-20 14:55:11 -0600

    Primer Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'babe3fe19064d93426681af98058e645'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd91ba9ca-47a7-46f7-a7dc-68bf6d97fb05'
+};
